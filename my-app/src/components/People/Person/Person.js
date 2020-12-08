48132cbbc1fb6049d5597a54b5d963da8a387fb0
krisi01 <chrisisbeast@abv.bg>
2020-12-08 04:23:39 -0800

    Fixed

--- my-app/src/components/People/Person/Person.js
+++ my-app/src/components/People/Person/Person.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import classes from './Person.module.css'
-
+import AuxComp from '../../../hoc/AuxComp';
 const Person = props =>
 {
     return (
-        <div className={classes.App}>
+        <AuxComp>
             <p>I am {props.username} and im { props.age} years old</p>
             <p onClick={props.clicked}>Click me to remove</p>
             <input type="text" onChange={props.changed} value={props.username}/>
-        </div>
+        </AuxComp>
     )
 }
 export default Person;

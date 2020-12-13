a69295d5b1c43bf8a82727f967ceca624022875f
krisi01 <chrisisbeast@abv.bg>
2020-12-13 11:49:24 -0800

    Posting a post using http requests and dummy server data

--- practiceajax/src/containers/Blog/Blog.js
+++ practiceajax/src/containers/Blog/Blog.js
@@ -4,15 +4,28 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
-    render () {
+    state = {
+        posts: []
+    }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const list = posts.map(post => {return {...post, 'Author': 'Chris'}});
+                this.setState({posts: list})
+                //console.log(response);
+            });
+    }
+    render() {
+        
+        const posts = this.state.posts.map(post => <Post key={post.key} title={post.title} author={post.Author}/>)
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />

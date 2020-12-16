ed1bfb942bc5c6789282836328b53f6eb53d8104
krisi01 <chrisisbeast@abv.bg>
2020-12-16 11:30:44 -0800

    Changes

--- practiceajax/src/containers/Blog/Blog.js
+++ practiceajax/src/containers/Blog/Blog.js
@@ -1,37 +1,11 @@
 import React, { Component } from 'react';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+
 import './Blog.css';
 import axios from 'axios';
-
+import Posts from '../Posts/Posts';
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null
-    }
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(response => {
-                const posts = response.data.slice(0,4);
-                const updatedPosts = posts.map(element => {
-                    return {...element, 'author': 'Krismata'}
-                });
-                this.setState({posts: updatedPosts});
-            });
-    }
-    showPost = (id) => {
-        this.setState({selectedPostId: id});
-    }
     render() {
-        
-        let posts = this.state.posts.map(post => {
-            return <Post title={post.title}
-                author={post.author}
-                key={post.id}
-                clicked={() => this.showPost(post.id)}/>
-        })
         return (
             <div className="Blog">
                 <header>
@@ -43,14 +17,8 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <section className="Posts">
-                {posts}
                 </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                <Posts />
             </div>
         );
     }
